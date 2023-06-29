96c299aad9ff54212176b5bafe03ef12bbc8a16b
gasher <robert@dock.io>
2023-06-29 18:10:46 +0300

    Fix UI on credit cards and reward program pages.

--- pages/rewards-program.js
+++ pages/rewards-program.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 
 import PageLayout from 'components/page-layout';
-import PageTitle from 'components/page-title';
 import RequireProof from 'components/require-proof';
 import { informations } from 'utils';
 
@@ -148,12 +147,9 @@ export default function RewardsProgram() {
   };
 
   return (
-    <PageLayout>
-      <PageTitle>
-        {!isCheckCompleted ? 'Dock Bank Freedom Rewards' : 'Checkout'}
-      </PageTitle>
+    <PageLayout title={!isCheckCompleted ? 'Dock Bank Freedom Rewards' : 'Checkout'}>
       {!isCheckCompleted && (
-        <h2 className="mb-6 font-bold text-gray-800 text-l">Dock Bank members receive 20% off</h2>
+        <h2 className="mb-6 font-bold text-center text-gray-800 text-l">Dock Bank members receive 20% off</h2>
       )}
       {!isSubmitted && !isCheckCompleted && (
         <RewardsProgramForm handleFormSubmit={handleFormSubmit} />
