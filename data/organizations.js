29c284b5ea65545d0ff98bf57a627cc07bb44425
dhipgraby <keneth.zambrano4@gmail.com>
2024-02-05 13:18:40 +0100

    Organizations page as Landing

---
+++ data/organizations.js
@@ -0,0 +1,39 @@
+const organizations = [
+    {
+        name: "Quotient",
+        logo: "/quotient.png",
+        description: "Quotient is our credit union",
+        url: "/org/quotient",
+        size: [168, 38]
+    },
+    {
+        name: "Identity Clarity",
+        logo: "/clarity.png",
+        description: "IdentityClarity is the KYC provider",
+        url: "/org/clarity",
+        size: [198, 120]
+    },
+    {
+        name: "FORSUR",
+        logo: "/forsur.png",
+        description: "Forsur is the Biomitric provider",
+        url: "/org/forsur",
+        size: [93, 82]
+    },
+    {
+        name: "Equinet",
+        logo: "/equinet.png",
+        description: "EquiNet is the credit bureau",
+        url: "/org/equinet",
+        size: [124, 89]
+    },
+    {
+        name: "Urbanscape",
+        logo: "/urbanscape.png",
+        description: "Urbanscape is the fictions Apartment company offering upscale rentals",
+        url: "/org/urbanscape",
+        size: [124, 107]
+    },
+]
+
+export default organizations
