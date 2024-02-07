db5a80481677f147ce9106507d90d39dd3c560a2
dhipgraby <keneth.zambrano4@gmail.com>
2024-02-07 17:37:41 +0100

    format check fix

--- pages/api/handle-credentials.js
+++ pages/api/handle-credentials.js
@@ -1,32 +1,32 @@
 export default async (req, res) => {
-    if (req.method !== 'POST') {
-        console.log('Only post request allowed');
-        res.status(400).json({});
-        return;
-    }
+  if (req.method !== 'POST') {
+    console.log('Only post request allowed');
+    res.status(400).json({});
+    return;
+  }
 
-    const body = req.body;
+  const body = req.body;
 
-    console.log('body::', body);
+  console.log('body::', body);
 
-    const result = await fetch(body.url, {
-        method: 'POST',
-        headers: {
-            accept: 'application/json',
-            'content-type': 'application/json',
-            Authorization: `Bearer ${process.env.DOCK_API_TOKEN}`,
-            'DOCK-API-KEY': process.env.DOCK_API_TOKEN,
-        },
-        body: JSON.stringify(body),
-    });
+  const result = await fetch(body.url, {
+    method: 'POST',
+    headers: {
+      accept: 'application/json',
+      'content-type': 'application/json',
+      Authorization: `Bearer ${process.env.DOCK_API_TOKEN}`,
+      'DOCK-API-KEY': process.env.DOCK_API_TOKEN,
+    },
+    body: JSON.stringify(body),
+  });
 
-    const response = await result.json();
-    console.log('response:', result);
+  const response = await result.json();
+  console.log('response:', result);
 
-    if (!result.ok) {
-        const errorText = await result.text();
-        throw new Error(`API Error: ${result.status} - ${errorText}`);
-    }
+  if (!result.ok) {
+    const errorText = await result.text();
+    throw new Error(`API Error: ${result.status} - ${errorText}`);
+  }
 
-    res.status(202).send(response);
+  res.status(202).send(response);
 };
--- pages/index.js
+++ pages/index.js
@@ -6,41 +6,35 @@ import { toast } from 'react-toastify';
 import PageLayout from 'components/page-layout';
 
 export default function Home() {
-    return (
-        <>
-            <PageLayout>
-                <div className='cardsContainer m-auto p-10 text-center'>
-                    <div className='flex'>
-                        <div className='mr-5'>
-                            <Image
-                                alt="docklogo"
-                                src="/docklogo.png"
-                                width={84}
-                                height={32}
-                            />
-                        </div>
-                        <div>
-                            <h1 className='Header'>
-
-                                <span className='mr-2'>|</span> Sales Demo</h1>
-                        </div>
-                    </div>
-                    <div className='text-left mt-5'>
-                        <h1 className='title'>Organizations</h1>
-                    </div>
-                    <div className='pt-5 grid lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 sm:grid-cols-1 gap-4'>
-                        {
-                            organizations.map((org, i) => <div key={i} className='orgCard'>
-                                    <OrganizationCard key={i} org={org} />
-                                </div>)
-                        }
-
-                    </div>
-                    <button className='launchBtn' onClick={() => toast.info('hello')} >
-                        Launch Demo
-                    </button>
-                </div>
-            </PageLayout>
-        </>
-    );
+  return (
+    <>
+      <PageLayout>
+        <div className="cardsContainer m-auto p-10 text-center">
+          <div className="flex">
+            <div className="mr-5">
+              <Image alt="docklogo" src="/docklogo.png" width={84} height={32} />
+            </div>
+            <div>
+              <h1 className="Header">
+                <span className="mr-2">|</span> Sales Demo
+              </h1>
+            </div>
+          </div>
+          <div className="text-left mt-5">
+            <h1 className="title">Organizations</h1>
+          </div>
+          <div className="pt-5 grid lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 sm:grid-cols-1 gap-4">
+            {organizations.map((org, i) => (
+              <div key={i} className="orgCard">
+                <OrganizationCard key={i} org={org} />
+              </div>
+            ))}
+          </div>
+          <button className="launchBtn" onClick={() => toast.info('hello')}>
+            Launch Demo
+          </button>
+        </div>
+      </PageLayout>
+    </>
+  );
 }
