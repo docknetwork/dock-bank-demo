e81d6370b28b73bdd710063d032cca48a0f30f9e
dhipgraby <keneth.zambrano4@gmail.com>
2024-02-10 19:12:01 +0100

    desktop design done for Equinet page

--- components/org/equinet/dataTable/index.jsx
+++ components/org/equinet/dataTable/index.jsx
@@ -8,7 +8,7 @@ function EquinetTable({ data }) {
   return (
     <div className='space-y-4'>
       <div className='flex items-center justify-between'>
-        <h2 className='font-semibold text-lg'>Credentials</h2>
+        <h2 className='font-semibold text-lg'>History</h2>
         <div className='px-2 flex space-x-1 items-center border rounded-lg'>
           <Search className='text-gray-400' />
           <Input className='w-1/3 border-0' placeholder='Search' />
--- components/org/equinet/sidebar.jsx
+++ components/org/equinet/sidebar.jsx
@@ -1,6 +1,8 @@
 import { cn } from 'utils';
 import { Button } from 'components/ui/button';
 import { CircleUserRound, LogOut } from 'lucide-react';
+import Image from 'next/image';
+import { ChevronDown } from 'lucide-react';
 
 /**
  * @description Equinet sidebar menu.
@@ -8,40 +10,40 @@ import { CircleUserRound, LogOut } from 'lucide-react';
  * @returns React.FC Sidebar
  */
 export function Sidebar({ className }) {
-    return (
-        <div className={cn('pb-6 bg-sky-800 w-full', className)}>
+    return (        
+        <div className={cn('pb-6 equinetBg w-full', className)}>
             <div className="h-full flex flex-col justify-between py-4 text-white">
                 <div>
                     <div className='p-2'>
-                        EQUINET LOGO HERE
+                        <Image src="/equinetlogo.png" width={256} height={40} alt='equinetlogo' />
                     </div>
                     <div className="px-3 py-2">
                         <div className="space-y-2">
-                            <Button variant="ghost" className="w-full justify-start">
+                            <Button variant="ghost" className="w-full justify-start rounded-full">
                                 Dashboard
                             </Button>
-                            <Button variant="secondary" className="w-full justify-start">
-                                Users
+                            <Button variant="gost" className="w-full justify-start rounded-full activeItem w-full">
+                                Users <span className='ta-r w-full'><ChevronDown /></span>
                             </Button>
-                            <Button variant="ghost" className="w-full justify-start">
+                            <Button variant="ghost" className="w-full justify-start rounded-full">
                                 Scores
                             </Button>
-                            <Button variant="ghost" className="w-full justify-start">
+                            <Button variant="ghost" className="w-full justify-start rounded-full">
                                 Designer
                             </Button>
-                            <Button variant="ghost" className="w-full justify-start">
+                            <Button variant="ghost" className="w-full justify-start rounded-full">
                                 Verification
                             </Button>
-                            <Button variant="ghost" className="w-full justify-start">
+                            <Button variant="ghost" className="w-full justify-start rounded-full">
                                 Organization profile
                             </Button>
-                            <Button variant="ghost" className="w-full justify-start">
+                            <Button variant="ghost" className="w-full justify-start rounded-full">
                                 Ecosystem
                             </Button>
-                            <Button variant="ghost" className="w-full justify-start">
+                            <Button variant="ghost" className="w-full justify-start rounded-full">
                                 Activities
                             </Button>
-                            <Button variant="ghost" className="w-full justify-start">
+                            <Button variant="ghost" className="w-full justify-start rounded-full">
                                 Plan and billing
                             </Button>
                         </div>
