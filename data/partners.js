4d1f23efb0cfa8d3687da714e4228d46ae486669
dhipgraby <keneth.zambrano4@gmail.com>
2024-02-11 00:11:36 +0100

    Partners page created

---
+++ data/partners.js
@@ -0,0 +1,39 @@
+const partners = [
+    {
+        logo: "/claritywhite.png",
+        url: "/partners/clarity",
+        description: "IdentityClarity is the KYC provider to the clarity partners ecosystem",
+        background: "#D19C3C",
+        sizes: [179, 49]
+    },
+    {
+        logo: "/forsurwhite.png",
+        url: "/partners/forsur",
+        description: "Forsur offers biometric credentials for such and such.",
+        background: "#3D4C41",
+        sizes: [93, 82]
+    },
+    {
+        logo: "/quotientwhite.png",
+        url: "/partners/quotient",
+        description: "Quotient Credit Union offers Banking, Home, and Auto Loans",
+        background: "#0E387A",
+        sizes: [164, 38]
+    },
+    {
+        logo: "/urbanwhite.png",
+        url: "/partners/urbanscape",
+        description: "Urbanscape offers upscale apartment homes for lease.",
+        background: "#35595B",
+        sizes: [124, 107]
+    },
+    {
+        logo: "/equinetwhite.png",
+        url: "/partners/equinet",
+        description: "EquiNET offers credit monitoring and credit scoring.",
+        background: "#265170",
+        sizes: [124, 89]
+    },
+]
+
+export default partners
