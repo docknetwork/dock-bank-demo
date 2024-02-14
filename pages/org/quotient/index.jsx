94b5c425ffa2c3f110a18f98a29d4dfef5f98989
Ivo G <41997352+Dadogg80@users.noreply.github.com>
2024-02-14 00:29:58 +0000

    fix: format-check failing because the identified lines did not adhere to the expected code style. Fixed!

--- pages/org/quotient/index.jsx
+++ pages/org/quotient/index.jsx
@@ -61,8 +61,12 @@ const QuotientBankForm = () => {
   useEffect(() => {
     const [govId, webcamPic] = [form.getValues('govId'), form.getValues('webcamPic')];
 
-    if (isCaptureCompleted && webcamPic === '') form.resetField('webcamPic', { defaultValue: '/example_webcam.png' });
-    if (isUploadPoDComplete && govId === '') form.resetField('govId', { defaultValue: '/example_passport.png' });
+    if (isCaptureCompleted && webcamPic === '') {
+      form.resetField('webcamPic', { defaultValue: '/example_webcam.png' });
+    }
+    if (isUploadPoDComplete && govId === '') {
+      form.resetField('govId', { defaultValue: '/example_passport.png' });
+    }
   }, [isCaptureCompleted, isUploadPoDComplete, form]);
 
   // once form values are valid, do something
