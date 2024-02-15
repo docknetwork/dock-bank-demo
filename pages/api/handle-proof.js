d8520a6c9151a73c83bb68b7100c77cd80db4cf4
dhipgraby <keneth.zambrano4@gmail.com>
2024-02-15 18:12:45 +0100

    fix lint and form-check

--- pages/api/handle-proof.js
+++ pages/api/handle-proof.js
@@ -1,29 +1,29 @@
 import { dockUrl } from 'utils/constants';
 
 export default async (req, res) => {
-    if (req.method !== 'GET') {
-        console.log('Only GET request allowed');
-        res.status(400).json({});
-        return;
-    }
+  if (req.method !== 'GET') {
+    console.log('Only GET request allowed');
+    res.status(400).json({});
+    return;
+  }
 
-    const PROOF_ID = req.query.proofID;
+  const PROOF_ID = req.query.proofID;
 
-    const result = await fetch(`${dockUrl}/proof-requests/${PROOF_ID}`, {
-        method: 'GET',
-        headers: {
-            'DOCK-API-TOKEN': `${process.env.NEXT_PUBLIC_DOCK_API_TOKEN}`,
-            'content-type': 'application/json',
-        }
-    });
+  const result = await fetch(`${dockUrl}/proof-requests/${PROOF_ID}`, {
+    method: 'GET',
+    headers: {
+      'DOCK-API-TOKEN': `${process.env.NEXT_PUBLIC_DOCK_API_TOKEN}`,
+      'content-type': 'application/json',
+    },
+  });
 
-    const response = await result.json();
-    console.log('response:', result);
+  const response = await result.json();
+  console.log('response:', result);
 
-    if (!result.ok) {
-        const errorText = await result.text();
-        throw new Error(`API Error: ${result.status} - ${errorText}`);
-    }
+  if (!result.ok) {
+    const errorText = await result.text();
+    throw new Error(`API Error: ${result.status} - ${errorText}`);
+  }
 
-    res.status(202).send(response);
+  res.status(202).send(response);
 };
