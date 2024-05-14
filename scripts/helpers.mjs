4f02f4982ca094cd51246977013e70b83acc6e0a
Mike Parkhill <mike@dock.io>
2024-05-14 16:26:52 -0400

    invite participants to ecosystem

--- scripts/helpers.mjs
+++ scripts/helpers.mjs
@@ -1,4 +1,16 @@
+import fs from 'node:fs/promises';
+
 export function scrubForFilename(original) {
   const match = /[\W,\\]/g;
   return original.replace(match, '_');
+}
+
+export async function readJSON(filename) {
+    let partDetails;
+    await fs.readFile(filename, 'utf8', (err, data) => {
+      if (err) throw err;
+      partDetails = JSON.parse(data);
+    });
+
+    return partDetails;
 }
--- scripts/setup-certs.mjs
+++ scripts/setup-certs.mjs
@@ -2,10 +2,11 @@ import axios from 'axios';
 import fs from 'node:fs/promises';
 import { EOL } from 'os';
 import { createRequire } from 'module';
-import { scrubForFilename } from './helpers.mjs';
+import { readJSON, scrubForFilename } from './helpers.mjs';
 
 const require = createRequire(import.meta.url);
 const profiles = require('./ecosystem-requests/clarity_partners_16/profiles/profiles.json');
+const ecosystem = require('./ecosystem-requests/clarity_partners_16/clarity_partners_16.json');
 
 const axiosHeaders = {
   headers: {
@@ -14,7 +15,9 @@ const axiosHeaders = {
 };
 
 const newEnvironment = {
-  DOCK_API_TOKEN: process.env.DOCK_API_TOKEN
+  DOCK_API_TOKEN: process.env.DOCK_API_TOKEN,
+  DOCK_API_URL: process.env.DOCK_API_URL,
+  NEXT_PUBLIC_DOCK_API_URL: process.env.DOCK_API_URL
 };
 
 async function writeEnvFile() {
@@ -46,25 +49,88 @@ async function createProfiles() {
     console.log(`\t ${profile.name}`);
 
     try {
-    const didResponse = await axios.post(didsUrl, {}, axiosHeaders);
+      const didResponse = await axios.post(didsUrl, {}, axiosHeaders);
 
-    console.log(JSON.stringify(didResponse.data, null, '\t'));
-    profile.did = didResponse.data.did;
-    const profileResponse = await axios.post(profilesUrl, profile, axiosHeaders);
-    populatedProfiles[scrubForFilename(profile.name)] = profileResponse.data;
+      profile.did = didResponse.data.did;
+      const profileResponse = await axios.post(profilesUrl, profile, axiosHeaders);
+      populatedProfiles[scrubForFilename(profile.name)] = profileResponse.data;
     } catch (error) {
       console.error(error);
     }
   });
 
   await Promise.all(requests);
-  console.log(JSON.stringify(populatedProfiles, null, '\t'));
   return populatedProfiles;
+}
+
+async function createEcosystem(adminDID) {
+    const ecosystemUrl = `${process.env.DOCK_API_URL}/trust-registries`;
+
+    console.log('--- Creating ecosystem ---');
+
+    // update details
+    ecosystem.convener = adminDID;
+
+    try {
+     const ecoResponse = await axios.post(ecosystemUrl, ecosystem, axiosHeaders);
+
+      return ecoResponse.data;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+}
+
+async function populateEcosystemParticipants(ecosystemId, participants) {
+    const inviteUrl = `${process.env.DOCK_API_URL}/trust-registries/${ecosystemId}/participants`;
+    const acceptUrl = `${process.env.DOCK_API_URL}/trust-registries/invitations/accept`;
+    
+    console.log('--- Inviting ecosystem participants ---');
+    const participantRequests = await Object.keys(participants).map(async (key) => {
+    try {
+      const participant = participants[key];
+      console.log(`\t${participant.name}`);
+      // get participant details from file
+      const details = await readJSON(`./ecosystem-requests/clarity_partners_16/participants/${key}.json`);
+      details.did = participant.did;
+
+      // create invite
+      const inviteBody = {
+        issuerSchemas: details.issuerSchemas,
+        verifierSchemas: details.verifierSchemas
+      };
+     const inviteResponse = await axios.post(inviteUrl, inviteBody, axiosHeaders);
+
+     // auto accept the invite
+     // NOTE: this should never be done in a production system - the participant should accept the invite
+     // themselves by clicking on the link
+        const inviteToken = inviteResponse.data.link.split('?token=')[1];
+        details.token = inviteToken;
+
+        const { data: acceptResult } = await axios.post(
+          acceptUrl,
+          details,
+          axiosHeaders
+        );
+
+      return acceptResult;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  });
+
+  await Promise.all(participantRequests);
 }
 
 export async function setupCerts() {
   const populatedProfiles = await createProfiles();
   newEnvironment.DOCK_API_DID = populatedProfiles.Quotient_Credit_Union.did;
+
+  const createdEcosystem = await createEcosystem(populatedProfiles.Quotient_Credit_Union.did);
+
+  await populateEcosystemParticipants(createdEcosystem.id, populatedProfiles);
+
   await writeEnvFile();
 }
 
