f566d00b6a5b68e2dcb9f1cbd27cc627759958a3
Mike Parkhill <mike-parkhill@users.noreply.github.com>
2024-07-30 17:39:19 -0400

    Equinet schema and KVAC support (#31)
    * update equinet with schema and kvack

--- utils/issue-crendentials.js
+++ utils/issue-crendentials.js
@@ -9,7 +9,7 @@ export const issueRevokableCredential = async (credential, setRevokableCredentia
 
     let registry = null;
     if (isRevocable) {
-        const type = credentialPayload.algorithm === 'dockbbs' ? 'DockVBAccumulator2022' : 'StatusList2021Entry';
+        const type = credentialPayload.algorithm === ('dockbbs' || 'bbdt16') ? 'DockVBAccumulator2022' : 'StatusList2021Entry';
         registry = await getExistingRegistry(_credential.issuer.id, type);
 
         if (!registry) {
