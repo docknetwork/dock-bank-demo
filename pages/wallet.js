708e453935cf8db4ddf95d31ae8b9543de3d5d1e
gasher <robert@dock.io>
2024-09-26 16:43:55 +0300

    Fix get wallet url.

--- pages/wallet.js
+++ pages/wallet.js
@@ -7,20 +7,20 @@ import { SERVER_URL } from 'utils/constants';
 export default function Wallet() {
   const { Canvas } = useQRCode();
   const qrCodeSize = 400;
-  const walletUrl = `https://${SERVER_URL}/api/getwallet`;
+  const walletUrl = `https://${SERVER_URL.replace('https://', '')}/api/getwallet`;
   return (
     <>
       <Head>
         <title>Quotient Wallet</title>
       </Head>
       <Header />
-      <div className="mb-4 mt-2">
-        <h2 className="font-medium text-3xl text-slate-700">Get the Quotient Wallet</h2>
+      <div className="mt-2 mb-4">
+        <h2 className="text-3xl font-medium text-slate-700">Get the Quotient Wallet</h2>
         <p className="text-xl">
           Scan the QR code to download the Quotient Wallet for Android or iOS.
         </p>
 
-        <div className="m-auto rounded-lg overflow-hidden flex justify-center">
+        <div className="flex justify-center m-auto overflow-hidden rounded-lg">
           <Canvas
             text={walletUrl}
             options={{
