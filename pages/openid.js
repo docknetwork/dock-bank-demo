b631f9845789655547e44f39f9225c5efd9e2b08
Sam Hellawell <sshellawell@gmail.com>
2025-03-20 19:24:55 +0000

    remove debug

--- pages/openid.js
+++ pages/openid.js
@@ -261,7 +261,6 @@ export default function Home() {
           ...credential,
           issuer: process.env.NEXT_PUBLIC_QUOTIENT_ISSUER_ID,
         },
-        format: 'sdjwt',
       },
       singleUse: true,
     });
